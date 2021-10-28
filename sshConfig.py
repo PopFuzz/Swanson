6f6710c5b31d5505c59ba873872578f5bec4f255
Roni Kaiser <popfuzz821@gmail.com>
2021-10-27 17:04:17 -0700

    I have no idea what I am doing anymore.

--- sshConfig.py
+++ sshConfig.py
@@ -5,26 +5,34 @@ import netmiko
 from netmiko import ConnectHandler
 from netmiko.ssh_exception import NetMikoTimeoutException
 from netmiko.ssh_exception import AuthenticationException
-from paramiko.ssh_exception import SSHException 
+from paramiko.ssh_exception import SSHException
 from getpass import getpass
+
+
 
 USERNAME = input('Please enter your SSH username : ')
 PASSWORD = getpass('Please enter your SSH password : ')
 
+
 device = {
-    'ip' : '192.168.108.10',
+    'ip' : '192.168.108.1',
     'username' : USERNAME,
     'password' : PASSWORD,
     'device_type' : 'cisco_ios'
 }
 
+
 try:
   c = ConnectHandler(**device)
+  output = c.send_command('show run')
+  f = open(f'backup_config','x')
+  f.write(output)
+  f.close
 except (NetMikoTimeoutException):
   print ("The following device has timed out: " + device['ip'])
 except (AuthenticationException):
   print ("Authentication failure on: " + device['ip'])
-except (ssh_exception):
+except (SSHException):
   print("Could not connect to the device using SSH protocol.  Check your SSH settings on: " + device['ip'])
 
 print("The script has completed")
